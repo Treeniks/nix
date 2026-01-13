952781774e915fdc85e62b787680a1ff3d67d0ee
Thomas Lindae <thomas@lindae.de>
2026-01-13 02:15:50 +0100

    sublime: disabled dotfiles management for now as it causes it to break

--- home-manager/common/sublime.nix
+++ home-manager/common/sublime.nix
@@ -4,6 +4,6 @@
     sublime4
     sublime-merge
   ];
-  xdg.configFile."sublime-text/Packages/User".source = "${inputs.dotfiles}/sublime/User";
+  # xdg.configFile."sublime-text/Packages/User".source = "${inputs.dotfiles}/sublime/User";
   xdg.configFile."sublime-text/Packages/Vinimum".source = inputs.sublime-vinimum;
 }
