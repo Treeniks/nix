828440e97e39d5c18b91b974b9b418ce5bd5d816
Thomas Lindae <thomas@lindae.de>
2026-01-15 22:17:34 +0100

    added env vars to disable llvmpipe

--- nixos/desktop/configuration.nix
+++ nixos/desktop/configuration.nix
@@ -38,4 +38,16 @@
     steam-run
     wineWowPackages.waylandFull
   ];
+
+  # NixOS's mesa drivers are quite monolithic and will in particular include the software "GPU" llvmpipe.
+  # This "disables" llvmpipe in hopes of preventing Steam from shitting itself.
+  # It probably does nothing though.
+  #
+  # `vulkaninfo --summary` however will now only show the real discrete GPU.
+  environment.variables = {
+    MESA_VK_DEVICE_SELECT = "1002:7550";
+    MESA_VK_DEVICE_SELECT_FORCE_DEFAULT_DEVICE = "1";
+
+    VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json:/run/opengl-driver-32/share/vulkan/icd.d/radeon_icd.i686.json";
+  };
 }
