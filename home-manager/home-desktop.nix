1fe45e8cb1ea0876d644b7baa1fd92f55f40d416
Thomas Lindae <thomas@lindae.de>
2026-01-18 01:51:38 +0100

    added comment on how to circumvent protonmail-desktop segfaulting on wayland

--- home-manager/home-desktop.nix
+++ home-manager/home-desktop.nix
@@ -6,6 +6,10 @@
   ];
 
   home.packages = with pkgs; [
+    # NOTE: This app segfaults on wayland. To fix, start it once with x11:
+    # XDG_SESSION_TYPE=x11 proton-mail
+    # Subsequent starts will then work with wayland.
+    # see https://github.com/NixOS/nixpkgs/issues/365156
     protonmail-desktop
     proton-pass
 
