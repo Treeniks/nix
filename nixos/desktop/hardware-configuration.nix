be4b871942daa25cc7fe79591994c73de834ec1b
Thomas Lindae <thomas@lindae.de>
2026-02-26 16:26:39 +0100

    desktop: new install onto new drive

--- nixos/desktop/hardware-configuration.nix
+++ nixos/desktop/hardware-configuration.nix
@@ -14,25 +14,25 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/698eca17-01e3-4ee1-aa1b-e18429c29a46";
+    { device = "/dev/disk/by-uuid/5f8c5909-4b97-4977-8344-8186af286b61";
       fsType = "btrfs";
       options = [ "compress=zstd" "subvol=@" ];
     };
 
   fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/698eca17-01e3-4ee1-aa1b-e18429c29a46";
+    { device = "/dev/disk/by-uuid/5f8c5909-4b97-4977-8344-8186af286b61";
       fsType = "btrfs";
       options = [ "compress=zstd" "subvol=@home" ];
     };
 
   fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/698eca17-01e3-4ee1-aa1b-e18429c29a46";
+    { device = "/dev/disk/by-uuid/5f8c5909-4b97-4977-8344-8186af286b61";
       fsType = "btrfs";
       options = [ "compress=zstd" "noatime" "subvol=@nix" ];
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/9701-EA94";
+    { device = "/dev/disk/by-uuid/6C1A-825F";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
