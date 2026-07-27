5bcadea3adb8b1075acc11bf69b599a59fae946f
Thomas Lindae <thomas@lindae.de>
2026-07-27 18:53:16 +0200

    default apps via xdg mime-types
    certainly doesn't cover everything, but having a way to configure this was on my TODO list for a while

--- modules/graphical/default.nix
+++ modules/graphical/default.nix
@@ -85,15 +85,58 @@
 
       xdg.mimeApps = {
         enable = true;
-        defaultApplications = {
-          "text/plain" = "Helix.desktop";
-          "text/html" = "brave-browser.desktop";
+        defaultApplications =
+          let
+            editor = "Helix.desktop";
+            browser = "brave-browser.desktop";
+            image = "org.gnome.eog.desktop";
+            video = "mpv.desktop";
+            audio = "mpv.desktop";
+            pdf = "org.gnome.Evince.desktop";
+          in
+          {
+            # see https://mimetype.io/all-types
+            "text/javascript" = editor;
+            "text/markdown" = editor;
+            "text/plain" = editor;
+            "text/x-asm" = editor;
+            "text/x-c" = editor;
+            "text/x-fortran" = editor;
+            "text/x-java-source" = editor;
+            "text/x-pascal" = editor;
+            "text/x-python" = editor;
 
-          "image/*" = "org.gnome.eog.desktop";
-          "video/*" = "mpv.desktop";
+            "text/html" = browser;
 
-          "application/pdf" = "org.gnome.Evince.desktop";
-        };
+            "image/bmp" = image;
+            "image/gif" = image;
+            "image/jpeg" = image;
+            "image/png" = image;
+            "image/tiff" = image;
+            "image/webp" = image;
+
+            "video/mp2t" = video; # .ts
+            "video/mp4" = video;
+            "video/mpeg" = video;
+            "video/ogg" = video;
+            "video/quicktime" = video;
+            "video/webm" = video;
+            "video/x-m4v" = video;
+            "video/x-matroska" = video;
+
+            "audio/aac" = audio;
+            "audio/flac" = audio;
+            "audio/mp4" = audio;
+            "audio/mpeg" = audio;
+            "audio/ogg" = audio;
+            "audio/opus" = audio;
+            "audio/wav" = audio;
+            "audio/webm" = audio;
+            "audio/x-matroska" = audio;
+            "audio/x-ms-wma" = audio;
+
+            "application/pdf" = pdf;
+          };
       };
 
       programs = {
